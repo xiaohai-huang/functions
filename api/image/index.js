95f9d2f03879f182ac490613953eb74f78836e0c
xiaohai-huang <jeromehuang18@gmail.com>
2023-05-03 03:05:38 +0000

    fix: save Cache-Control header in cache

--- api/image/index.js
+++ api/image/index.js
@@ -5,15 +5,23 @@ import NodeCache from "node-cache";
 const imageCache = new NodeCache({ stdTTL: 604800, useClones: false });
 
 export default async function handler(req, res) {
-  const { url, width, height, format = "origin", quality = 1 } = req.query;
-  let buffer = imageCache.get(url);
-  let cacheControl;
-  if (!buffer) {
-    const response = await fetch(url);
-    buffer = await response.arrayBuffer();
-    cacheControl = response.headers.get("cache-control");
-    imageCache.set(url, buffer);
+  const {
+    url: rawUrl,
+    width,
+    height,
+    format = "origin",
+    quality = 1,
+  } = req.query;
+  const url = decodeURIComponent(rawUrl);
+
+  let response = imageCache.get(url);
+  if (!response) {
+    response = await fetch(url);
+    const cacheControl = response.headers.get("Cache-Control");
+    const buffer = await response.arrayBuffer();
+    imageCache.set(url, { cacheControl, buffer });
   }
+  const { buffer, cacheControl } = imageCache.get(url);
 
   const image = sharp(buffer);
   const metadata = await image.metadata();
